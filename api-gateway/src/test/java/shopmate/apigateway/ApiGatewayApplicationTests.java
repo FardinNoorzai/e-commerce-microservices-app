700f12f7b05f073ae978130e081bb6e07761f043
noorzai <fardin.noorzai321@gmail.com>
2025-02-18 17:29:19 +0430

    .

---
+++ api-gateway/src/test/java/shopmate/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package shopmate.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service-discovery/src/test/java/shopmate/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package shopmate.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
